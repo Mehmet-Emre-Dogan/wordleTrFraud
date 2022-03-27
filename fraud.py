3adf7c4969b9307631f51a220e4aa9d2b11b8cb0
Mehmet Emre Doğan <87245315+Mehmet-Emre-Dogan@users.noreply.github.com>
2022-03-27 22:01:43 +0300

    added yellow letter processing feature

--- fraud.py
+++ fraud.py
@@ -26,18 +26,27 @@ print(f"Yüklü kelime sayısı: {WORD_COU}")
 while True:
     try:
         print("="*50)
-        print("Bildiğiniz harfleri yazın. Bilmedikleriniz yerine '.' koyun. \nSonra bir boşluk bırakıp engelli harfleri yan yana yazın. \nÖrnek: a.a.a şkl [cevap araba ama r ve b yi henüz bulamadınız; ş k ve l nin de olmadığı ortaya çıktı]")
+        print("Bildiğiniz harfleri yazın. Bilmedikleriniz yerine '.' koyun. \nSonra bir boşluk bırakıp engelli harfleri yan yana yazın. \nSonra bir boşluk daha bırakıp sarı harfleri yazın. \nÖzellik kullanmak istemiyorsanız '*' yazın. \nÖrnek: a.a.a şkl b [cevap araba ama r yi henüz hiç bulamadınız ve b yi de sarı olarak buldunuz; ş k ve l nin de olmadığı ortaya çıktı] \nÖrnek2: a.a.a şkl * [cevap araba ama r ve b yi henüz hiç bulamadınız; ş k ve l nin de olmadığı ortaya çıktı; sarı harf çıkmadı]")
         myStrArr = input("--> ").split()
-        if len(myStrArr) <= 1:
-            myStrArr.append("")
+
         if DEBUG:
             print(myStrArr)
         compildeRegex = re.compile(myStrArr[0])
         posiibleWords = list(set(filter(compildeRegex.match, wordArr))) # set is here to get rid of duplicates
-        prohibitedLetters = list(myStrArr[1])
+
+        if myStrArr[1] != '*':
+            prohibitedLetters = list(myStrArr[1])
+        else:
+            prohibitedLetters = []
+
+        if myStrArr[2] != '*':
+            existingLetters = list(myStrArr[2])
+        else:
+            existingLetters = []    
+
         if DEBUG:
             print(prohibitedLetters)
-        posiibleWords = naturalSort([word for word in posiibleWords if not any(letter in word for letter in prohibitedLetters)])
+        posiibleWords = naturalSort([word for word in posiibleWords if ( (not any(letter in word for letter in prohibitedLetters)) and all(letter in word for letter in existingLetters) )])
         if posiibleWords:
             for i, word in enumerate(posiibleWords):
                 print(f"{str(i+1).zfill(3)}- {word}")
